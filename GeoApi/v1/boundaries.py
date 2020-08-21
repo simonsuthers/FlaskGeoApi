c20c20b8ce70f3ea659c761df01d5d4cc65e4e61
Simon Suthers <simonsuthers@yahoo.co.uk>
2020-08-21 17:19:12 +0100

    Added test geopandas routine

--- GeoApi/v1/boundaries.py
+++ GeoApi/v1/boundaries.py
@@ -1,7 +1,18 @@
 from .. import app
 from flask import jsonify
+import pandas as pd
+import geopandas as gpd
 
 @app.route("/boundary")
 def boundary():
-    return jsonify("hello", "user")
+
+    df = pd.DataFrame(
+    {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+     'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+     'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+     'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]})
+
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+
+    return gdf.head().to_json()
 
